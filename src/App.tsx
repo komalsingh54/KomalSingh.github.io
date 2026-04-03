582bff143966fa05e5c1022d58bf5fa89340e2ea
Komal <komalsingh54@gmail.com>
2026-04-03 10:55:17 +0100

    Configure site for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/KomalSingh.github.io">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
